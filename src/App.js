1aaeeab00fdbf60ddd95828cf1ad189562e2b4bf
Tristan <tantris.kantner@gmail.com>
2021-05-22 10:03:28 +0200

    Create basic functionality

--- src/App.js
+++ src/App.js
@@ -1,33 +1,104 @@
 import { useEffect, useState } from 'react';
-import { DeleteField } from './DeleteButton';
-import { GuestField } from './PushButton';
 
 export function App() {
   const baseUrl = 'https://guestlistapi.herokuapp.com/';
 
   const [guestList, setGuestList] = useState([]);
-
-  // const getGuestList = async () => {
-  //   const response = await fetch(`${baseUrl}`);
-  //   const allGuests = await response.json();
-  //   console.log(allGuests);
-  // };
+  const [fetch1, setFetch1] = useState(true);
+  const [listUpdate, setListUpdate] = useState(true);
+  const [firstName, setFirstName] = useState('Santa');
+  const [lastName, setLastName] = useState('Claus');
 
   useEffect(() => {
-    fetch(`${baseUrl}`).then((x) =>
-      x.json().then((data) => setGuestList(data)),
+    fetch(`${baseUrl}`).then(
+      (x) => x.json().then((data) => setGuestList(data)),
+      setFetch1(false),
     );
-  }, []);
-  if (guestList.length === 0) {
+  }, [listUpdate]);
+
+  if (fetch1) {
     return <div>Loading ....</div>;
   }
   return (
     <div>
-      {console.log(guestList)}
-      <button> Hello Guest List</button>
-
-      <GuestField />
-      {/* <DeleteField /> */}
+      <div>
+        <form>
+          <input
+            type="text"
+            placeholder="Enter First Name"
+            onChange={(e) => {
+              setFirstName(e.currentTarget.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Enter Last Name"
+            onChange={(e) => {
+              setLastName(e.currentTarget.value);
+            }}
+          />
+          <button
+            onClick={async (e) => {
+              e.preventDefault();
+              await fetch(`${baseUrl}`, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                  firstName: { firstName },
+                  lastName: { lastName },
+                }),
+              });
+              setListUpdate(!listUpdate);
+            }}
+          >
+            {' '}
+            Add Guest{' '}
+          </button>
+        </form>
+      </div>
+      <ul>
+        {guestList.map((guest) => {
+          return (
+            <li key={guest.id}>
+              Guest {guest.id}: {'            '}
+              {guest.lastName.lastName}
+              {'            '}
+              {guest.firstName.firstName}
+              {'            '}
+              status:{guest.attending ? ' attending' : ' not coming'}
+              {'            '}
+              <button
+                onClick={async () => {
+                  await fetch(`${baseUrl}${guest.id}`, {
+                    method: 'DELETE',
+                  });
+                  setListUpdate(!listUpdate);
+                }}
+              >
+                {' '}
+                Delete Guest{' '}
+              </button>
+              <button
+                onClick={async () => {
+                  await fetch(`${baseUrl}${guest.id}`, {
+                    method: 'PATCH',
+                    headers: {
+                      'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ attending: !guest.attending }),
+                  });
+                  setListUpdate(!listUpdate);
+                }}
+              >
+                {' '}
+                Update Status{' '}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
