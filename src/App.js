5b51c38be872d8f23837a6ea060f8216aca00069
Tristan <tantris.kantner@gmail.com>
2021-05-25 16:12:49 +0200

    Separate into different Components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { useEffect, useState } from 'react';
+import { FilterButtons } from './FilterButtons';
+import GuestInputField from './GuestInputField';
+import GuestList from './GuestList';
 
 export function App() {
   const baseUrl = 'https://guestlistapi.herokuapp.com/';
@@ -7,13 +10,15 @@ export function App() {
   const [newGuestList, setNewGuestList] = useState([]);
   const [fetch1, setFetch1] = useState(true);
   const [listUpdate, setListUpdate] = useState(true);
-  const [firstName, setFirstName] = useState('Santa');
-  const [lastName, setLastName] = useState('Claus');
-  const [filter, setFilter] = useState('all');
+  const [filter, setFilter] = useState('all Guests:');
 
   useEffect(() => {
     fetch(`${baseUrl}`).then(
-      (x) => x.json().then((data) => setGuestList(data)),
+      (x) =>
+        x.json().then((data) => {
+          setGuestList(data);
+          setNewGuestList(data);
+        }),
       setFetch1(false),
     );
   }, [listUpdate]);
@@ -23,232 +28,30 @@ export function App() {
   }
   return (
     <div>
-      <div>
-        <p>branch</p>
-        <form>
-          <input
-            type="text"
-            placeholder="Enter First Name"
-            onChange={(e) => {
-              setFirstName(e.currentTarget.value);
-            }}
-          />
-          <input
-            type="text"
-            placeholder="Enter Last Name"
-            onChange={(e) => {
-              setLastName(e.currentTarget.value);
-            }}
-          />
-          <button
-            onClick={async (e) => {
-              e.preventDefault();
-              await fetch(`${baseUrl}`, {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                  firstName: { firstName },
-                  lastName: { lastName },
-                }),
-              });
-              setListUpdate(!listUpdate);
-              if (filter === 'none') {
-                setFilter('all');
-              }
-            }}
-          >
-            {' '}
-            Add Guest{' '}
-          </button>
-        </form>
-      </div>
-      {filter === 'none' && (
-        <div>
-          <p>No Guests in the List yet.</p>
-        </div>
-      )}
-      {filter === 'all' && (
-        <div>
-          <ul>
-            {guestList.map((guest) => {
-              return (
-                <li key={guest.id}>
-                  Guest {guest.id}: {'            '}
-                  {guest.lastName.lastName}
-                  {'            '}
-                  {guest.firstName.firstName}
-                  {'            '}
-                  status:{guest.attending ? ' attending' : ' not coming'}
-                  {'            '}
-                  <button
-                    onClick={async () => {
-                      await fetch(`${baseUrl}${guest.id}`, {
-                        method: 'DELETE',
-                      });
-                      setListUpdate(!listUpdate);
-                    }}
-                  >
-                    {' '}
-                    Delete Guest{' '}
-                  </button>
-                  <button
-                    onClick={async () => {
-                      await fetch(`${baseUrl}${guest.id}`, {
-                        method: 'PATCH',
-                        headers: {
-                          'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify({ attending: !guest.attending }),
-                      });
-                      setListUpdate(!listUpdate);
-                    }}
-                  >
-                    {' '}
-                    Update Status{' '}
-                  </button>
-                </li>
-              );
-            })}
-          </ul>
-          <p>Number of Guests: {guestList.length}</p>
-        </div>
-      )}
-      {filter === 'attending' && (
-        <div>
-          <ul>
-            {newGuestList.map((guestatt) => {
-              return (
-                <li key={guestatt.id}>
-                  Guest {guestatt.id}: {'            '}
-                  {guestatt.lastName.lastName}
-                  {'            '}
-                  {guestatt.firstName.firstName}
-                  {'            '}
-                  status:{guestatt.attending ? ' attending' : ' not coming'}
-                  {'            '}
-                  <button
-                    onClick={async () => {
-                      await fetch(`${baseUrl}${guestatt.id}`, {
-                        method: 'DELETE',
-                      });
-                      setListUpdate(!listUpdate);
-                    }}
-                  >
-                    {' '}
-                    Delete Guest{' '}
-                  </button>
-                  <button
-                    onClick={async () => {
-                      await fetch(`${baseUrl}${guestatt.id}`, {
-                        method: 'PATCH',
-                        headers: {
-                          'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify({
-                          attending: !guestatt.attending,
-                        }),
-                      });
-                      setListUpdate(!listUpdate);
-                    }}
-                  >
-                    {' '}
-                    Update Status{' '}
-                  </button>
-                </li>
-              );
-            })}
-          </ul>
-          <p>Number of Guests: {guestList.length}</p>
-        </div>
-      )}
-      {filter === 'notAttending' && (
-        <div>
-          <ul>
-            {newGuestList.map((guestNatt) => {
-              return (
-                <li key={guestNatt.id}>
-                  Guest {guestNatt.id}: {'            '}
-                  {guestNatt.lastName.lastName}
-                  {'            '}
-                  {guestNatt.firstName.firstName}
-                  {'            '}
-                  status:{guestNatt.attending ? ' attending' : ' not coming'}
-                  {'            '}
-                  <button
-                    onClick={async () => {
-                      await fetch(`${baseUrl}${guestNatt.id}`, {
-                        method: 'DELETE',
-                      });
-                      setListUpdate(!listUpdate);
-                    }}
-                  >
-                    {' '}
-                    Delete Guest{' '}
-                  </button>
-                  <button
-                    onClick={async () => {
-                      await fetch(`${baseUrl}${guestNatt.id}`, {
-                        method: 'PATCH',
-                        headers: {
-                          'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify({
-                          attending: !guestNatt.attending,
-                        }),
-                      });
-                      setListUpdate(!listUpdate);
-                    }}
-                  >
-                    {' '}
-                    Update Status{' '}
-                  </button>
-                </li>
-              );
-            })}
-          </ul>
-          <p>Number of Guests: {guestList.length}</p>
-        </div>
-      )}
-      <div>
-        <p>Filter by: </p>
-        <button onClick={() => setFilter('all')}>All</button>
-        <button
-          onClick={() => {
-            setFilter('notAttending');
-            setNewGuestList(
-              guestList.filter((value) => value.attending === false),
-            );
-          }}
-        >
-          Not Attending
-        </button>
-        <button
-          onClick={() => {
-            setFilter('attending');
-            setNewGuestList(
-              guestList.filter((value) => value.attending === true),
-            );
-          }}
-        >
-          Attending
-        </button>
-        <button
-          onClick={async () => {
-            guestList.map(async (guest) => {
-              await fetch(`${baseUrl}${guest.id}`, {
-                method: 'DELETE',
-              });
-              setGuestList([]);
-            });
-            setListUpdate(!listUpdate);
-            setFilter('all');
-          }}
-        >
-          Delete All
-        </button>
-      </div>
+      <GuestInputField
+        baseUrl={baseUrl}
+        listUpdate={listUpdate}
+        setListUpdate={setListUpdate}
+        onClick={() => setListUpdate(!listUpdate)}
+      />
+
+      <GuestList
+        baseUrl={baseUrl}
+        listUpdate={listUpdate}
+        setListUpdate={setListUpdate}
+        newGuestList={newGuestList}
+        filter={filter}
+      />
+      <FilterButtons
+        baseUrl={baseUrl}
+        listUpdate={listUpdate}
+        setListUpdate={setListUpdate}
+        newGuestList={newGuestList}
+        setFilter={setFilter}
+        setNewGuestList={setNewGuestList}
+        setGuestList={setGuestList}
+        guestList={guestList}
+      />
     </div>
   );
 }
